b92d059fc0d0567add6a213febc821700367759d
sebastiandan <sebastiandan@EVO235.evointern>
2015-03-10 15:59:54 +0200

    // My request, checkboxes apply filter request data driven

---
+++ src/test/java/com/MyRequestsFilterDataDrivenTest.java
@@ -0,0 +1,48 @@
+package com;
+
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.ManagedPages;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.pages.Pages;
+import net.thucydides.junit.annotations.UseTestDataFrom;
+import net.thucydides.junit.runners.ThucydidesParameterizedRunner;
+import net.thucydides.junit.runners.ThucydidesRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import com.steps.LogInSteps;
+import com.steps.MyRequestsSteps;
+
+@RunWith(ThucydidesParameterizedRunner.class)
+@UseTestDataFrom("Resources/filterTestData.csv")
+public class MyRequestsFilterDataDrivenTest {
+	
+	@Managed(uniqueSession = true)
+	public WebDriver webdriver;
+	
+	String Vacation_Type,Days_Number,Vacation_Status;
+
+	@ManagedPages(defaultUrl = "http://192.168.1.68:9090")
+	public Pages pages;
+
+	@Steps
+	public LogInSteps logIn;
+	@Steps
+	public MyRequestsSteps reqSteps;
+
+	@Test
+	public void filter_and_check_filtered_data() {
+		logIn.is_the_login_page();
+		logIn.entersUsername("dana.zaharia");
+		logIn.entersPassword("danna");
+		logIn.clickLogIn();
+		logIn.clickVacation();
+		reqSteps.set_vacation_type(Vacation_Type);
+		reqSteps.set_daysnumber_type(Days_Number);
+		reqSteps.set_vacationstatus_type(Vacation_Status);
+		reqSteps.click_apply_filter_button();
+	}
+
+}
