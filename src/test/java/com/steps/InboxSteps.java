66b064f2e564e841cb256dad52aa140e7d99bba5
danielazaharia <danielazaharia@EVO333.evointern>
2015-03-11 14:57:26 +0200

    verify the workdays number from tabel

--- src/test/java/com/steps/InboxSteps.java
+++ src/test/java/com/steps/InboxSteps.java
@@ -1,5 +1,9 @@
 package com.steps;
 
+import java.text.ParseException;
+
+import org.junit.Assert;
+
 import com.pages.InboxPage;
 
 import net.thucydides.core.annotations.Step;
@@ -33,12 +37,23 @@ public class InboxSteps extends ScenarioSteps{
 	}
 	
 	@Step
+	public int workdays_between_endDate_and_startDate(String start, String end) throws ParseException {
+		return inboxPage.workdays_between_endDate_and_startDate( start, end);
+	}
+	
+
+	@Step
+	public void check_if_selected_request_was_approved(String employee, String startDate){
+		inboxPage.check_if_selected_request_was_approved(employee, startDate);
+	}
+	
+	@Step
 	public void click_approve_button(){
 		inboxPage.click_approve_button();
 	}
 	
 	@Step
-	public void check_if_selected_request_was_approved(String employee, String startDate){
-		inboxPage.check_if_selected_request_was_approved(employee, startDate);
+	public void check_if_number_of_workdays_is_correct(String start, String end, String employee) throws ParseException{
+		Assert.assertEquals("Workdays number is not correct!", inboxPage.workdays_between_endDate_and_startDate(start, end), inboxPage.get_daysNumber_from_table(employee, start));
 	}
 }
