0601337173795dc6709bbd9efcd7b3d30d0e1a74
sebastiandan <sebastiandan@EVO235.evointern>
2015-03-12 17:50:22 +0200

    // Open web page modified

--- src/main/java/com/pages/OpenWebPage.java
+++ src/main/java/com/pages/OpenWebPage.java
@@ -1,18 +1,29 @@
 package com.pages;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
+
+import net.thucydides.core.annotations.findby.By;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class OpenWebPage {
 
 	public static void main(String[] args) throws ClientProtocolException, IOException {
+		
+		
 		//String url = "http://www.google.com/search?q=car";
 		String url = "https://www.google.ro/search?q=car&gws_rd=cr,ssl&ei=8IkBVdL0OvON7AbJsYCYBg#q=car";
 
@@ -35,5 +46,29 @@ public class OpenWebPage {
 			result.append(line);
 			System.out.println(line);
 		}
+		OpenWebPage.OpenFirsLink(args);	
+	
+}
+	
+	public static void OpenFirsLink (String[] args) {
+
+	    WebDriver driver = new FirefoxDriver();
+	    driver.get("http://www.google.com");
+	    WebElement element = driver.findElement(By.name("q"));
+	    element.sendKeys("car!\n"); // send also a "\n"
+	    element.submit();
+
+	    // wait until the google page shows the result
+	    WebElement myDynamicElement = (new WebDriverWait(driver, 10))
+	             .until(ExpectedConditions.presenceOfElementLocated(By.id("resultStats")));
+	    String path = "ol[id='rso'] h3[class='r'] a";
+	    driver.findElements(By.cssSelector(path)).get(0);
+	    	    
+	    
+	    // this are all the links you like to visit
+	    //for (WebElement webElement : path)
+	   // 	{
+	       // System.out.println(webElement.getAttribute("href"));
+	    //	}
 	}
 }
